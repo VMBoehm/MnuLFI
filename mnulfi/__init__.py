76c1ecaeda76e1ffeb1615ee70307c2cafb04956
Francois <francois@orioncloud.fr>
2019-10-06 20:52:54 -0700

    Removes the  need for pesky environment variables

--- mnulfi/__init__.py
+++ mnulfi/__init__.py
@@ -1,3 +1,7 @@
-from . import util as UT
+import os
 
-UT.check_env() 
+# Path to data files required for cosmos
+_MNULFI_DATA_DIR=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+
+def dat_dir():
+    return _MNULFI_DATA_DIR
